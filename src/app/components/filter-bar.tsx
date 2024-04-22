5e60eb5769eb777831efa1b11074c0076f9c936f
Hariharan Selvaraj <hariharan.2309.s@icloud.com>
2024-04-22 22:01:56 +0200

    more filter bar changes - WIP

--- src/app/components/filter-bar.tsx
+++ src/app/components/filter-bar.tsx
@@ -1,38 +1,51 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { ICountry, IProduct } from '../types';
 import styles from './filter-bar.module.css';
 
 interface FilterBarProps {
   countries: ICountry;
   products: IProduct[];
+  getAverage: (filter: Filter) => void;
 }
 
-interface FilterProps {
+interface Filter {
   country: string;
   product: string;
   fromDate: string;
   toDate: string;
 }
 
-export const FilterBar = ({ countries, products }: FilterBarProps) => {
-  const [filter, setFilter] = useState<FilterProps | null>(null);
+export const FilterBar = ({ countries, products, getAverage }: FilterBarProps) => {
+  const [filter, setFilter] = useState<Filter>({
+    country: 'DE',
+    product: 'methane',
+    fromDate: '2019-02-01',
+    toDate: '2019-03-01',
+  });
 
   const onCountryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    console.log('eeee ', e.target.value);
-    console.log('selected value ', countries[e.target.value]);
+    setFilter((filter) => ({
+      ...filter,
+      country: e.target.value,
+    }));
   };
 
-  useEffect(() => {
-    setFilter({
-      country: 'DE',
-      product: 'methane',
-      fromDate: '',
-      toDate: '',
-    });
-    console.log('using effect!!!');
-  }, [countries, products]);
+  const onProductChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setFilter((filter) => ({
+      ...filter,
+      product: e.target.value,
+    }));
+  };
+
+  const onFromDateChange = (e: React.ChangeEvent<HTMLInputElement>) => { };
+
+  const onToDateChange = (e: React.ChangeEvent<HTMLInputElement>) => { };
+
+  const handleClick = () => {
+    getAverage(filter);
+  };
 
   return (
     <div>
@@ -41,7 +54,7 @@ export const FilterBar = ({ countries, products }: FilterBarProps) => {
         id="country-select"
         defaultValue="DE"
         onChange={onCountryChange}
-        className={styles.country}
+        className={styles.select}
       >
         {Object.entries(countries).map((country: any) => (
           <option key={country[0]} value={country[0]}>
@@ -49,6 +62,25 @@ export const FilterBar = ({ countries, products }: FilterBarProps) => {
           </option>
         ))}
       </select>
+      <select
+        name="product-select"
+        id="product-select"
+        defaultValue="methane"
+        onChange={onProductChange}
+        className={styles.select}
+      >
+        {products.map((product: IProduct) => (
+          <option key={product.name} value={product.name} style={{}}>
+            {product.name}
+          </option>
+        ))}
+      </select>
+      <input type="date" name="fromDate" className={styles.date} value={filter?.fromDate} onChange={onFromDateChange} />
+      <input type="date" name="toDate" className={styles.date} value={filter?.toDate} onChange={onToDateChange} />
+
+      <button onClick={handleClick} className={styles.show}>
+        Show me
+      </button>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { ICountry, IProduct } from './types';
 
 const PRODUCTS_API_URL = 'https://api.v2.emissions-api.org/api/v2/products.json';
 const COUNTRIES_API_URL = 'https://api.v2.emissions-api.org/api/v2/countries.json';
+const AVERAGE_API_URL = 'https://api.v2.emissions-api.org/api/v2';
 
 const getEmissionsProducts = async () => {
   const res = await fetch(PRODUCTS_API_URL);
@@ -25,17 +26,34 @@ const getCountries = async () => {
   return res.json();
 };
 
+const getAverage = async (filter: any) => {
+  'use server';
+
+  const res = await fetch(
+    `${AVERAGE_API_URL}/${filter.product}/average.json?country=${filter.country}&begin=${filter.fromDate}&end=${filter.toDate}`
+  );
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch average data!');
+  }
+
+  console.log('getting average...');
+
+  return res.json();
+};
+
 export default async function Home() {
   const products: IProduct[] = await getEmissionsProducts();
   const countries: ICountry = await getCountries();
 
   return (
     <main className={styles.main}>
-      <FilterBar products={products} countries={countries} />
+      <FilterBar products={products} countries={countries} getAverage={getAverage} />
       <div className={styles.grid}>
         {products?.map((product: IProduct) => (
           <div key={product.name} className={styles.card}>
-            {product.name}
+            <span>{product.name}</span>
+            <p>{product.description.replace(', total column', '.')}</p>
           </div>
         ))}
       </div>
